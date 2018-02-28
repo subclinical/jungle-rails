3f1d30a2ac303dbeb49dfc66d1d3561b275c70f6
Danny K Pan <kaiwen006@gmail.com>
2018-02-28 23:29:54 +0000

    added order list to show end point of orders controller email is also listed on summary page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @line_items = LineItem.where(order_id: @order.id)
+    puts @line_items
+  
   end
 
   def create
