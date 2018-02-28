92821acc07e05ccb535f54fe6cf00f93e6d4fd6c
Danny K Pan <kaiwen006@gmail.com>
2018-02-28 16:44:35 +0000

    added sold-out feature for products with quantity of 0

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,7 +55,7 @@ cat1.products.create!({
   name:  'Hipster Hat',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('apparel3.jpg'),
-  quantity: 4,
+  quantity: 0,
   price: 34.49
 })
 
@@ -104,7 +104,7 @@ cat2.products.create!({
   name:  'World\'s Largest Smartwatch',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('electronics3.jpg'),
-  quantity: 32,
+  quantity: 0,
   price: 2_026.29
 })
 
